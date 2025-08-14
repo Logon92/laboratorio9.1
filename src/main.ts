d5e115ad793a14e3a4b3db73bc7b1bd43f36077a
Logon92 <you@example.com>
2025-08-14 21:28:37 +0200

    cambios finales

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { inicializarSelectProductos, calcularTicket } from "./ui";
 
 function allowNumbersOnly(e: KeyboardEvent): boolean {
-    if (!/^\d$/.test(e.key)) {
-        e.preventDefault();
-        return false;
-    }
-    return true;
+	if (!/^\d$/.test(e.key)) {
+		e.preventDefault();
+		return false;
+	}
+	return true;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    inicializarSelectProductos();
-    (window as any).calcularTicket = calcularTicket;
-    (window as any).AllowNumbersOnly = allowNumbersOnly;
+document.addEventListener("DOMContentLoaded", () => {
+	inicializarSelectProductos();
+	calcularTicket();
+	(window as any).calcularTicket = calcularTicket;
+	(window as any).AllowNumbersOnly = allowNumbersOnly;
 });
